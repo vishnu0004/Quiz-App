4d56b40da443a0256d81d093d5572a5a5459844c
badagaud <vishnugaud315@gmail.com>
2025-02-14 08:57:00 +0530

    add code

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Online Quiz System</h1>
+   <a href="add_quiz.php"><button>Add Questions</button></a> 
+    <a href="quiz.php"><button>Start Quiz</button></a>
+</body>
+</html>
