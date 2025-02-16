27000f2bd1ed7cc9066dc48424119bb218c8680d
badagaud <vishnugaud315@gmail.com>
2025-02-16 08:02:39 +0530

    modify

--- php/index.php
+++ php/index.php
@@ -15,6 +15,6 @@
 <body>
     <h1>Online Quiz System</h1>
    <a href="add_quiz.php"><button>Add Questions</button></a> 
-    <a href="quiz.php?n=2"><button>Start Quiz</button></a>
+    <a href="quiz.php?n=1"><button>Start Quiz</button></a>
 </body>
 </html>
